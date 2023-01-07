5d506be1e205161f832a0eb59584e2241afc49fc
thunder-godd <iammonytony8@gmail.com>
2023-01-07 16:39:53 +0300

    1639

--- pages/Post/index.jsx
+++ pages/Post/index.jsx
@@ -1,42 +1,102 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { collection, addDoc, getDocs, doc } from "firebase/firestore";
+import { db, storage } from "../../utils/firebase";
+
 const Post = () => {
-	const [listing, setListing] = useState({});
+	const [listings, setListings] = useState({});
+	const [name, setName] = useState([]);
+	const [bedrooms, setBedrooms] = useState([]);
+	const [bathrooms, setBathrooms] = useState([]);
+	const [price, setPrice] = useState([]);
+	const [coverImage, setCoverImage] = useState();
+	useEffect(() => {
+		const getListings = async () => {
+			try {
+				const data = await getDocs(collection(db, "listings"));
+				setListings(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+				console.log(listings);
+			} catch (e) {
+				console.error("Error adding document: ", e);
+			}
+		};
+		getListings();
+	}, []);
+	console.log(listings);
 	const changeHandler = (e) => {
 		let selected = e.target.files[0];
 		const types = ["image/jpeg", "image/jpg"];
 		console.log(selected.name);
 		if (selected && types.includes(selected.types)) {
-			setListing((prev) => {
-				prev.cover_image = selected;
-			});
+			setCoverImage(selected);
 		}
 		console.log(listing);
+	};
+	const createListing = async () => {
+		try {
+			const listingsRef = await addDoc(collection(db, "listings"), {
+				name: name,
+				bedrooms: bedrooms,
+				bathrooms: bathrooms,
+				price: price,
+			});
+			console.log("Document written with ID: ", listingsRef.id);
+		} catch (e) {
+			console.error("Error adding document: ", e);
+		}
+	};
+
+	const updateListing = async () => {
+		try {
+			console.log("updating Listing");
+		} catch (e) {
+			console.error("Error adding document: ", e);
+		}
 	};
 
 	return (
 		<div>
 			<form>
 				<div>
-					<label>name</label>
-					<input type="text" />
+					<input
+						type="text"
+						placeholder="name"
+						onChange={(e) => setName(e.target.value)}
+					/>
 				</div>
 				<div>
-					<label>bedrooms</label>
-					<input type="text" />
+					<input
+						type="text"
+						placeholder="bedrooms"
+						onChange={(e) => setBedrooms(e.target.value)}
+					/>
 				</div>
 				<div>
-					<label>bathrooms</label>
-					<input type="text" />
+					<input
+						type="text"
+						placeholder="bathrooms"
+						onChange={(e) => setBathrooms(e.target.value)}
+					/>
 				</div>
 				<div>
-					<label>payment period</label>
-					<input type="text" />
+					<input
+						type="text"
+						placeholder="price"
+						onChange={(e) => setPrice(e.target.value)}
+					/>
 				</div>
 				<div>
 					<label>Cover Image</label>
 
 					<input type="file" onChange={(e) => changeHandler(e)} />
 				</div>
+				<button
+					onClick={(e) => {
+						e.preventDefault();
+						createListing();
+					}}>
+					Create
+				</button>
 			</form>
 			<div></div>
 		</div>
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore,collection, addDoc } from "firebase/firestore"
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCsx9v5k069FYFf0g4g5zsydScBqdlReLE",
-  authDomain: "househunters-d9361.firebaseapp.com",
-  projectId: "househunters-d9361",
-  storageBucket: "househunters-d9361.appspot.com",
-  messagingSenderId: "1023599326109",
-  appId: "1:1023599326109:web:fe56bfe61e55b68a223574"
-  storageBucket:"gs://househunters-d9361.appspot.com"
+	apiKey: "AIzaSyCsx9v5k069FYFf0g4g5zsydScBqdlReLE",
+	authDomain: "househunters-d9361.firebaseapp.com",
+	projectId: "househunters-d9361",
+	storageBucket: "househunters-d9361.appspot.com",
+	messagingSenderId: "1023599326109",
+	appId: "1:1023599326109:web:fe56bfe61e55b68a223574",
+	storageBucket: "gs://househunters-d9361.appspot.com",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db=getFirestore(app)
-const storage = getStorage(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 // try {
 //   const listingsRef = await addDoc(collection(db, "listings"), {
